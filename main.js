bca81ee2fd9949c0b3b6524c3eba0ceae20f745b
Harshad <hp1004032@gmail.com>
2021-07-10 10:52:50 +0530

    Registration Form

---
+++ main.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	checkInputs();
+});
+
+function checkInputs() {
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();	
+
+	if(passwordValue.length <= 8) {
+		setErrorFor(password, 'Password Must Be 8 Characters');
+	} else {
+		setSuccessFor(password);
+	}
+
+	if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
